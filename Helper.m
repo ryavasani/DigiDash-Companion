0c1dcdb3dd4fd6f376a8706687e3a80b73c8c7c7
Rushil Yavasani <59315865+ryavasani@users.noreply.github.com>
2025-09-08 23:54:36 -0500

    Update Helper.m

--- Helper.m
+++ Helper.m
@@ -1,10 +1,39 @@
 #import "ExampleHelper.h"
 #import <UIKit/UIKit.h>
+#import <sys/utsname.h>
 
 @implementation ExampleHelper
 
 + (NSString *)getSystemVersion {
     return [[UIDevice currentDevice] systemVersion];
+}
+
++ (NSString *)getDeviceModel {
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    return [NSString stringWithCString:systemInfo.machine
+                              encoding:NSUTF8StringEncoding];
+}
+
++ (NSNumber *)getBatteryLevel {
+    UIDevice *device = [UIDevice currentDevice];
+    device.batteryMonitoringEnabled = YES;
+    float level = device.batteryLevel;
+    if (level < 0) {
+        return @(-1); // unknown
+    }
+    return @(level * 100); // percentage
+}
+
++ (NSDictionary *)getStorageInfo {
+    NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil];
+    NSNumber *freeSize = [attributes objectForKey:NSFileSystemFreeSize];
+    NSNumber *totalSize = [attributes objectForKey:NSFileSystemSize];
+
+    return @{
+        @"free": freeSize ?: @(0),
+        @"total": totalSize ?: @(0)
+    };
 }
 
 @end
